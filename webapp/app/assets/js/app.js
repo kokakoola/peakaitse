ecca9bae8665250caae6417a45ce8d9666b0b8b4
mare hunt <mare.hunt@netgroup.ee>
2015-07-17 23:41:13 +0300

    design step2

--- webapp/app/assets/js/app.js
+++ webapp/app/assets/js/app.js
@@ -18893,9 +18893,10 @@ hh:"%d hours",d:"a day",dd:"%d days",M:"a month",MM:"%d months",y:"a year",yy:"%
     var updateChart = function(chartId, unit) {
         function getChartData(){
             $.ajax({
-                url: 'https://xenon.netgroupdigital.com:8443/mainfuse/data',
+                // url: 'https://xenon.netgroupdigital.com:8443/mainfuse/data',
+                url: '../data/longtest.json',
                 dataType: 'json',
-                type: 'POST',
+                // type: 'POST',
             })
             .done(function(data){
                 console.log(data);
@@ -18960,7 +18961,7 @@ hh:"%d hours",d:"a day",dd:"%d days",M:"a month",MM:"%d months",y:"a year",yy:"%
                     show: false
                 },
                 size: {
-                    height: 600
+                    height: 500
                 },
                 // color: {
                 //     pattern: ['url(#blueGradient)']
--- webapp/assets/js/app.js
+++ webapp/assets/js/app.js
@@ -64,9 +64,10 @@
     var updateChart = function(chartId, unit) {
         function getChartData(){
             $.ajax({
-                url: 'https://xenon.netgroupdigital.com:8443/mainfuse/data',
+                // url: 'https://xenon.netgroupdigital.com:8443/mainfuse/data',
+                url: '../data/longtest.json',
                 dataType: 'json',
-                type: 'POST',
+                // type: 'POST',
             })
             .done(function(data){
                 console.log(data);
@@ -131,7 +132,7 @@
                     show: false
                 },
                 size: {
-                    height: 600
+                    height: 500
                 },
                 // color: {
                 //     pattern: ['url(#blueGradient)']
